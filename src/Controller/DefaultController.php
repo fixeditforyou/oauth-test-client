2c5321bd47dec7e7d99bb8adbf0b3b29fc83c5be
Alexander Hofbauer <a.hofbauer@fify.at>
2021-12-02 19:50:15 +0100

    Demonstrate access_token/refresh_token workflow

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,6 +5,8 @@ declare(strict_types = 1);
 namespace App\Controller;
 
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Token\AccessToken;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,12 +15,35 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route(path: '/')]
 class DefaultController extends AbstractController
 {
+    private const CLIENT_NAME = 'e4o';
+
     #[Route(name: 'index')]
     public function index(
         Request $request,
+        ClientRegistry $clientRegistry,
     ): Response {
+        $session = $request->getSession();
+        /** @var AccessToken $accessToken */
+        $accessToken = $session->get('access_token');
+
+        $data = null;
+
+        if ($accessToken) {
+            $client = $clientRegistry->getClient(self::CLIENT_NAME);
+
+            if ($accessToken->hasExpired()) {
+                $refreshToken = $accessToken->getRefreshToken();
+                $accessToken  = $client->refreshAccessToken($refreshToken);
+
+                $session->set('access_token', $accessToken);
+            }
+
+            $user = $client->fetchUserFromToken($accessToken);
+            $data = $user->toArray();
+        }
+
         return $this->render('index.html.twig', [
-            'userdata' => $request->getSession()->get('userdata'),
+            'userdata' => $data,
         ]);
     }
 
@@ -29,18 +54,21 @@ class DefaultController extends AbstractController
     ): Response {
         $request->getSession()->clear();
 
-        return $clientRegistry->getClient('e4o')->redirect([], []);
+        return $clientRegistry->getClient(self::CLIENT_NAME)->redirect([], []);
     }
 
-    #[Route(path:'/redirect-uri', name: 'oauth_redirect_uri')]
+    /**
+     * @throws IdentityProviderException
+     */
+    #[Route(path: '/redirect-uri', name: 'oauth_redirect_uri')]
     public function oauthRedirectUri(
         Request $request,
         ClientRegistry $clientRegistry,
     ): Response {
-        $client = $clientRegistry->getClient('e4o');
+        $client      = $clientRegistry->getClient(self::CLIENT_NAME);
+        $accessToken = $client->getAccessToken();
 
-        $user = $client->fetchUser();
-        $request->getSession()->set('userdata', $user->toArray());
+        $request->getSession()->set('access_token', $accessToken);
 
         return $this->redirectToRoute('index');
     }
